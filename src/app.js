764f4734b916e3a773abf58b252cde33b1e6f647
Safar <101888055+safaarrr@users.noreply.github.com>
2026-08-02 14:46:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import apiRoutes from "./routes/api.js";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -10,12 +12,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Public Folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Home Route
+// API Routes
+app.use("/api", apiRoutes);
+
+// Dashboard
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+
+    res.sendFile(
+        path.join(__dirname, "public", "index.html")
+    );
+
 });
 
 export default app;
