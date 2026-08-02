027423325866369f7a71927af3424feead3f30af
Safar <101888055+safaarrr@users.noreply.github.com>
2026-08-02 14:38:01 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Public Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home Route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+export default app;
