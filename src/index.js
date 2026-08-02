be42182fdc67428e74a8ef57134a7a81e3e5f30c
Safar <101888055+safaarrr@users.noreply.github.com>
2026-08-02 14:37:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config();
 
-const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.send(`
-        <h1>${process.env.BOT_NAME}</h1>
-        <p>🚀 WhatsApp Bot is Running...</p>
-    `);
-});
-
 app.listen(PORT, () => {
-    console.log("================================");
-    console.log(`${process.env.BOT_NAME} Started`);
-    console.log(`Server Running on Port ${PORT}`);
-    console.log("================================");
+    console.log("====================================");
+    console.log(`🚀 ${process.env.BOT_NAME} Started`);
+    console.log(`🌐 Server : http://localhost:${PORT}`);
+    console.log("====================================");
 });
