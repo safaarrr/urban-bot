72637e2869923262cfd210b8bf8c264606d43184
Safar <101888055+safaarrr@users.noreply.github.com>
2026-08-05 21:11:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
 import { connectWhatsApp } from "./bot/connect.js";
 import app from "./app.js";
+import connectDatabase from "./config/database.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("====================================");
+app.listen(PORT, async () => {
+    console.log("==================================");
     console.log(`🚀 ${process.env.BOT_NAME} Started`);
     console.log(`🌐 Server : http://localhost:${PORT}`);
-    console.log("====================================");
+    console.log("==================================");
+
+    await connectDatabase();
     connectWhatsApp();
 });
