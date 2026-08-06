380789a0913c1f88b18dff386b42e61e7db5f3ed
Safar <101888055+safaarrr@users.noreply.github.com>
2026-08-06 15:05:29 +0530

    Update mongoAuth.js

--- src/config/mongoAuth.js
+++ src/config/mongoAuth.js
@@ -1,4 +1,4 @@
-import makeMongoAuthState from "mongo-baileys";
+import { useMongoDBAuthState } from "mongo-baileys";
 import { MongoClient } from "mongodb";
 
 let cached = null;
@@ -13,10 +13,9 @@ export async function getMongoAuthState() {
     console.log("✅ MongoDB Auth Ready");
 
     const db = client.db("urban-sync");
+    const collection = db.collection("baileys_auth");
 
-    const { state, saveCreds } = await makeMongoAuthState(
-        db.collection("baileys_auth")
-    );
+    const { state, saveCreds } = await useMongoDBAuthState(collection);
 
     cached = { state, saveCreds };
     return cached;
